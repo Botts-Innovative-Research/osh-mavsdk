55f697987474d55106b9d9244053b70b84fae437
micstinso <michael.stinson@botts-inc.com>
2025-07-24 18:40:40 -0500

    Fix mavproxy config already existing

--- comm/sensorhub-comm-mavlink2/containers/ardupilot/ardupilot_distrobox.sh
+++ comm/sensorhub-comm-mavlink2/containers/ardupilot/ardupilot_distrobox.sh
@@ -18,4 +18,4 @@ podman build -f ardupilot/Dockerfile -t ardupilot:v0.1 ./ardupilot &&
 distrobox-create --name ardupilot --image localhost/ardupilot:v0.1
 distrobox enter ardupilot -- bash -c "export HOME=/home/ardupilot && cd / && ./ardupilot/Tools/environment_install/install-prereqs-ubuntu.sh -y"
 
-distrobox enter ardupilot -- bash -c "export HOME=/home/ardupilot && mkdir /home/ardupilot/.mavproxy && touch ~/.mavproxy/mavinit.scr && echo 'output add 127.0.0.1:14540' >> ~/.mavproxy/mavinit.scr && echo 'output add 127.0.0.1:14560' >> ~/.mavproxy/mavinit.scr && echo 'output add 127.0.0.1:14550' >> ~/.mavproxy/mavinit.scr"
+distrobox enter ardupilot -- bash -c "export HOME=/home/ardupilot && mkdir -p /home/ardupilot/.mavproxy && touch ~/.mavproxy/mavinit.scr && echo 'output add 127.0.0.1:14540' >> ~/.mavproxy/mavinit.scr && echo 'output add 127.0.0.1:14560' >> ~/.mavproxy/mavinit.scr && echo 'output add 127.0.0.1:14550' >> ~/.mavproxy/mavinit.scr"
