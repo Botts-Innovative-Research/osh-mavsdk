31098b594d7e18fff87323a818a0528eb4cc7fec
micstinso <michael.stinson@botts-inc.com>
2025-07-23 19:51:24 -0500

    adding example container run.

--- comm/sensorhub-comm-mavlink2/containers/ardupilot/run.sh
+++ comm/sensorhub-comm-mavlink2/containers/ardupilot/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # run the ardupilot container
-distrobox enter ardupilot
+distrobox enter ardupilot -- bash -c "source ~/venv-ardupilot/bin/activate && cd ardupilot && ./Tools/autotest/sim_vehicle.py -v ArduCopter -f quad --console --map --instance 0 --sysid 1 --out udp:127.0.0.1:14551"
